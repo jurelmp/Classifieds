a3de41e9de5cff73cb58fde4b6b56efaf6200505
Jurel P <patocjurel@gmail.com>
2017-08-21 23:32:56 +0800

    Add foundation compiled assets

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,17 @@
 </head>
 <body>
 
-
+<div class="row">
+    <div class="medium-6 columns">
+        <h2>Our Agency, our selves.</h2>
+        <p>
+            Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.
+        </p>
+    </div>
+    <div class="medium-6 columns">
+        <img class="thumbnail" src="https://placehold.it/750x350" alt="Thumbnail">
+    </div>
+</div>
 
 </body>
 
